c85350ad76366f3125d76685a219accaa56672db
yang8320-dot <yang8320@gmail.com>
2026-05-02 14:03:24 +0800

    Update Program.cs

--- MiniProgram01/Program.cs
+++ MiniProgram01/Program.cs
@@ -1,16 +1,26 @@
 using System;
 using System.Windows.Forms;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 public static class Program {
     // 宣告一個全域的 Mutex，確保單一執行個體
     private static Mutex mutex = new Mutex(true, "{A93B72C1-D264-48E3-8E5F-B4A1F6C8D9E2}");
 
+    // --- 註冊自訂的 Windows 訊息，用於喚醒視窗 ---
+    [DllImport("user32.dll", SetLastError = true)]
+    static extern uint RegisterWindowMessage(string lpString);
+    
+    [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+    static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+    
+    public static readonly uint WM_SHOWME = RegisterWindowMessage("WM_SHOW_MINIPROGRAM01");
+    public static readonly IntPtr HWND_BROADCAST = new IntPtr(0xffff);
+
     [STAThread] 
     public static void Main() { 
         if (mutex.WaitOne(TimeSpan.Zero, true)) {
             try {
-                // 【DPI 升級】使用現代的 PerMonitorV2 確保在 125%, 150% 縮放時文字與介面完美清晰
                 Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
                 Application.EnableVisualStyles(); 
                 Application.SetCompatibleTextRenderingDefault(false);
@@ -23,7 +33,9 @@ public static class Program {
                 mutex.ReleaseMutex();
             }
         } else {
-            return; // 已經在執行中，退出
+            // 【優化】如果程式已在執行，廣播喚醒訊號給第一台程式，讓它彈到最上層
+            PostMessage(HWND_BROADCAST, WM_SHOWME, IntPtr.Zero, IntPtr.Zero);
+            return;
         }
     }
 }
